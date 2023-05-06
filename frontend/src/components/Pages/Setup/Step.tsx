682bde58b28374ef4b554e584d0bca4894f12066
Younes Arroubi <yarroubi@e1r2p12.1337.ma>
2023-05-07 00:11:04 +0100

    ui done

--- frontend/src/components/Pages/Setup/Step.tsx
+++ frontend/src/components/Pages/Setup/Step.tsx
@@ -13,7 +13,7 @@ const Step: React.FC<IStepProps> = ({ index, Illustartion, text, attachement }) 
 		<article className="flex flex-col items-center gap-4">
 			<h2 className="text-xs font-bold text-white bg-primary rounded px-2 py-1 ">{index}</h2>
 			<Illustartion />
-			<div className="flex flex-col gap-2">
+			<div className="flex flex-col gap-2 items-center">
 				<p className="text-xs text-primary font-medium text-center max-w-[250px]">{text}</p>
 				{attachement && <ClipBoardField text={attachement} />}
 			</div>
