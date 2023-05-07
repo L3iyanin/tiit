56b46a92c3532ebbd486ec124fb200f5b5ba9c32
IMRAN BAALI <56740152+kirwako@users.noreply.github.com>
2023-05-07 03:08:31 +0100

    (ops) add redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,18 +1,20 @@
-cd /home/ubuntu/tiit/frontend
+cd /home/ubuntu/tiit
 git pull
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-cd /home/ubuntu/tiit/frontend/frontend
+cd /home/ubuntu/tiit/frontend
 npm i && npm run build
-sudo rm -rf /var/www/html/bringo_frontend
-sudo mv /home/ubuntu/tiit/frontend/frontend/dist /var/www/html/bringo_frontend
+sudo rm -rf /var/www/html/tiit_frontend
+sudo mv /home/ubuntu/tiit/frontend/dist /var/www/html/tiit_frontend
 
-cd /home/ubuntu/tiit/frontend/backend
+cd /home/ubuntu/tiit/backend
+sudo rm -rf dist
 npm i
+npx prisma db push
 npx prisma migrate deploy
-npx prisma generate
+npx prisma db push
 pm2 delete tiit_backend
 npm run build
 pm2 start dist/main.js --name "tiit_backend"
